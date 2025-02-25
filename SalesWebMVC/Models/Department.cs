5ba5ff7e3f22fa23fe2224940b2270a1b68d06e1
Guilherme Garcia <guilherme.garcia@gabster.com.br>
2025-02-25 20:10:23 -0300

    Code review

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -1,8 +1,13 @@
-﻿namespace SalesWebMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SalesWebMVC.Models
 {
     public class Department
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
         public String Name { get; set; }
         public  ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
